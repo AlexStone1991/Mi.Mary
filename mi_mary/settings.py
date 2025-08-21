69814316ba98d72473d65b15e28277268a689249
ALEXSTONE <solo-drive@bk.ru>
2025-08-21 22:42:10 +0300

    mi_mary - Обновил название базы данных для проекта!

--- mi_mary/settings.py
+++ mi_mary/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'barbershop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'Mi_Mary_Base.sqlite3',
     }
 }
 
