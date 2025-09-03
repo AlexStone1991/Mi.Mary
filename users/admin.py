740e8d3e1e9574ab2fc6e03047ce59b48750f56d
ALEXSTONE <solo-drive@bk.ru>
2025-09-03 22:04:24 +0300

    mi_mary - добавил админку пользователь! и оформление чуть!

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone', 'birthday', 'vk_id', 'tg_id', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительная информация', {'fields': ('avatar', 'phone', 'birthday', 'vk_id', 'tg_id')}),
+    )
+
